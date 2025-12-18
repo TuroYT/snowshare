111e9b6346b8b5c9343fc0f0e85e4e2e46b1c821
Romain PINSOLLE <romain.pins@gmail.com>
2025-12-18 19:04:51 +0100

    no warn

--- src/app/(shares)/p/[slug]/page.tsx
+++ src/app/(shares)/p/[slug]/page.tsx
@@ -8,6 +8,7 @@ import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-json";
 import "prismjs/components/prism-markup";
+import "prismjs/components/prism-markup-templating";
 import "prismjs/components/prism-bash";
 import "prismjs/components/prism-c";
 import "prismjs/components/prism-cpp";
