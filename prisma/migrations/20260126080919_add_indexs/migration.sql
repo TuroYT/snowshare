f5aa3363427e8f7e43addffe7fa52a4741c3e73d
Romain PINSOLLE <romain.pinsolle@safrangroup.com>
2026-01-26 09:13:22 +0100

    littles preformance improvement

---
+++ prisma/migrations/20260126080919_add_indexs/migration.sql
@@ -0,0 +1,14 @@
+-- CreateIndex
+CREATE INDEX "Share_slug_idx" ON "Share"("slug");
+
+-- CreateIndex
+CREATE INDEX "Share_ownerId_idx" ON "Share"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "Share_createdAt_idx" ON "Share"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Share_expiresAt_idx" ON "Share"("expiresAt");
+
+-- CreateIndex
+CREATE INDEX "Share_type_createdAt_idx" ON "Share"("type", "createdAt");
