04caaed566bb3013bfeea8965a8bd6ccba35021b
Romain PINSOLLE <romain.pins@gmail.com>
2026-04-05 19:59:27 +0200

    fix docker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import NextAuthProvider from "@/components/NextAuthProvider";
 import { ThemeInitializer } from "@/components/ThemeInitializer";
@@ -12,15 +13,8 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 export const revalidate = 0; // Disable caching for metadata
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = GeistSans;
+const geistMono = GeistMono;
 
 export async function generateMetadata(): Promise<Metadata> {
   const data = await getPublicSettings();
